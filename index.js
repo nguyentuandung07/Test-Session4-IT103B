28b063777116aaee1a3c591f0d9822b3eeeee96b
nguyentuandung07 <chisatomod@gmail.com>
2026-02-02 13:30:43 +0700

    Test

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ else console.log("Giá trị n không hợp lệ để tạo dãy số");
 // Câu 2:
 let sum = 0;
 for (let i = 1; i <= 50; i++) {
-  if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz");
-  else if (i % 5 === 0) console.log("Buzz");
-  else if (i % 3 === 0) {
+  if (i % 3 === 0) {
     console.log("Fizz");
     sum++;
-  } else console.log(i);
+  } else if (i % 5 === 0) console.log("Buzz");
+  else if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz");
+  else console.log(i);
 }
 console.log(`Tổng của tất cả các chữ số đã in ra chữ "Fizz" là: ${sum}`);
